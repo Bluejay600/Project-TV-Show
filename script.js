df84b1a324224c54fdb0f00db15c24ff89c1f572
Waleed Yahya <waleedbluejay7@gmail.com>
2025-08-08 02:33:57 +0100

    deleted function which clears the episodes.

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
+  displayEpisodes(episodeList);
+}
+
+function displayEpisodes(episodes) {
   const rootElem = document.getElementById("root");
   rootElem.innerHTML = ""; // Clear previous content
 
@@ -54,33 +58,7 @@ searchInput.addEventListener("input", function () {
    displayEpisodes(filteredEpisodes);
   searchCount.textContent = `Showing ${filteredEpisodes.length} / ${allEpisodes.length} episodes`;
 });
-function displayEpisodes(episodes) {
-  const rootElem = document.getElementById("root");
-  rootElem.innerHTML = ""; // Clear previous content
 
-  episodes.forEach((episode) => {
-    const episodeCard = document.createElement("div");
-    episodeCard.className = "episode-card";
-    
-    const title = document.createElement("h3");
-    title.textContent = `${episode.name} — ${formatEpisodeCode(episode.season, episode.number)}`;
-
-    const image = document.createElement("img");
-    image.src = episode.image.medium;
-
-    const summary = document.createElement("div");
-    summary.innerHTML = episode.summary;
-
-    const link = document.createElement("a");
-    link.href = episode.url;
-    link.textContent = "View on TVMaze";
-    link.target = "_blank";
-
-    
-    episodeCard.append(title, image, summary, link);
-    rootElem.appendChild(episodeCard);
-  });
-}
 const episodeSelect = document.getElementById("episode-select");
 
 function populateEpisodeSelect(episodes) {
